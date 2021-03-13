95a66f562889078778b84bd3b0d9ae05bdbf6424
osmanturkarslan <79464729+osmanturkarslan@users.noreply.github.com>
2021-03-13 23:01:25 +0200

    Update FirstClass.java
    second commit from remote osman

--- src/package1/FirstClass.java
+++ src/package1/FirstClass.java
@@ -11,6 +11,8 @@ public class FirstClass {
 		//i will conflict my remote master branch
 
 		//Hey guys this is our remote master branch
+		//here a serious conflict I create
+		//there is the last part of my conflict
 
 	}
 }
